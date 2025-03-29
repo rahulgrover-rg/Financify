61294bf38dbb3e3f74b87b032ffe9cb90f8ba8f4
rahulgrover-rg <rahulgrover9373@gmail.com>
2025-03-29 10:14:21 +0530

    Add Header Component

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import { Header } from "@/components/header";
+
+type Props = {
+    children: React.ReactNode;
+}
+
+const DashboardLayout = ({children}: Props) => {
+  return (
+    <>
+        <Header/>
+        <main>
+            {children}
+        </main>
+    </>
+  )
+}
+
+export default DashboardLayout
