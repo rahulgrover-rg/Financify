d9d7f4bfce395b49ee189fa18e11525aece11d66
rahulgrover-rg <rahulgrover9373@gmail.com>
2025-04-01 23:58:40 +0530

    Add Transaction api endpoint

---
+++ features/transactions/api/use-bulk-delete-transactions.ts
@@ -0,0 +1,26 @@
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.transactions["bulk-delete"]["$post"]>;
+type ResquestType = InferRequestType<typeof client.api.transactions["bulk-delete"]["$post"]>["json"];
+
+export const useBulkDeleteTransactions = () => {
+    const queryClient = useQueryClient() ;
+    const mutation = useMutation<ResponseType,Error,ResquestType>({
+        mutationFn: async(json) => {
+            const response = await client.api.transactions["bulk-delete"]["$post"]({json});
+            return await response.json();
+        },
+        onSuccess: () => {
+            toast.success("Transactions deleted.")
+            queryClient.invalidateQueries({ queryKey: ["transactions"]}) ;
+        },
+        onError: () => {
+            toast.error("Failed to delete transactions")
+        },
+    });
+
+    return mutation;
+}
